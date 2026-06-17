a49d0982b244fd1e88a7ded0d40c98917bd6df7b
Michael Hughes <microneesia79@gmail.com>
2026-06-16 23:41:58 -0700

    Switch web entrypoint to v0.6 app shell

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Dice5, ScrollText, Backpack, Map, Sparkles } from 'lucide-react';
 import './styles.css';
-import App from './App.jsx';
+import AppV6 from './AppV6.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App icons={{ Dice5, ScrollText, Backpack, Map, Sparkles }} />
+    <AppV6 icons={{ Dice5, ScrollText, Backpack, Map, Sparkles }} />
   </React.StrictMode>
 );
