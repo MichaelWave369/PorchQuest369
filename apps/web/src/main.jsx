5ba6bb5cdc6ba456f843e1471a044532a42c707c
Michael Hughes <microneesia79@gmail.com>
2026-06-17 00:12:59 -0700

    Switch Pages entrypoint to AppV8

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Dice5, ScrollText, Backpack, Map, Sparkles } from 'lucide-react';
 import './styles.css';
 import './v7.css';
-import AppV7 from './AppV7.jsx';
+import './v8.css';
+import AppV8 from './AppV8.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppV7 icons={{ Dice5, ScrollText, Backpack, Map, Sparkles }} />
+    <AppV8 />
   </React.StrictMode>
 );
