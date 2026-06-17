d8ccbec3339097835847d693691c7ece307a1114
Michael Hughes <microneesia79@gmail.com>
2026-06-17 00:22:17 -0700

    Load v0.7.2 app shell

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './styles.css';
 import './v7.css';
 import './v8.css';
-import AppV8 from './AppV8.jsx';
+import './v9.css';
+import AppV9 from './AppV9.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppV8 />
+    <AppV9 />
   </React.StrictMode>
 );
